d0b656b41130b7b726e84f8088ab17e625a0fc2f
Luis E. Obando <luiseduardo.obando@gmail.com>
2015-11-16 00:16:01 -0500

    Add other version of SRM 158 DIV 2 class problem

--- SRM 158/DIV 2/TireRotation.cpp
+++ SRM 158/DIV 2/TireRotation.cpp
@@ -8,32 +8,70 @@
  */
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+// Other version of the class
+class TireRotation {
+public:
+    int getCycle(string initial, string current){
+        string firstCycle  = initial;
+        string secondCycle = next(firstCycle);
+        string thirdCycle  = next(secondCycle);
+        string fourthCycle = next(thirdCycle);
+        
+        if (current == firstCycle){
+            return 1;
+        }
+        else if (current == secondCycle){
+            return 2;
+        }
+        else if (current == thirdCycle) {
+            return 3;
+        }
+        else if (current == fourthCycle) {
+            return 4;
+        }
+        else {
+            return -1;
+        }
+    }
+private:
+    string next(string str){
+        string result = "";
+        result += str[3];
+        result += str[2];
+        result += str[0];
+        result += str[1];
+        
+        return result;
+        }
+};
+
+// First version of the class
+/*
 class TireRotation {
 public:
     int getCycle(string initial, string current){
         if (initial == current){
             return 1;
         }
-        else if (initial.at(0) == current.at(2) ||
-                 initial.at(1) == current.at(3) ||
-                 initial.at(2) == current.at(1) ||
-                 initial.at(3) == current.at(0)){
+        else if (initial[0] == current[2] ||
+                 initial[1] == current[3] ||
+                 initial[2] == current[1] ||
+                 initial[3] == current[0]){
             return 2;
         }
-        else if (initial.at(0) == current.at(1) ||
-                 initial.at(1) == current.at(0) ||
-                 initial.at(2) == current.at(3) ||
-                 initial.at(3) == current.at(2)){
+        else if (initial[0] == current[1] ||
+                 initial[1] == current[0] ||
+                 initial[2] == current[3] ||
+                 initial[3] == current[2]){
             return 3;
         }
-        else if (initial.at(0) == current.at(3) ||
-                 initial.at(1) == current.at(2) ||
-                 initial.at(2) == current.at(0) ||
-                 initial.at(3) == current.at(1)){
+        else if (initial[0] == current[3] ||
+                 initial[1] == current[2] ||
+                 initial[2] == current[0] ||
+                 initial[3] == current[1]){
             return 4;
         }
         else{
@@ -41,6 +79,7 @@ public:
         }
     }
 };
+*/
 
 int main(){
     TireRotation tireRotationTest;
