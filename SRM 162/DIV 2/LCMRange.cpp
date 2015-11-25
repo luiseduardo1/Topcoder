0e8d85f5ef7cd8ef82da2edbd40d3e907b40b9bb
Luis E. Obando <luiseduardo.obando@gmail.com>
2015-11-25 00:00:43 -0500

    Update LCMRange.cpp

--- SRM 162/DIV 2/LCMRange.cpp
+++ SRM 162/DIV 2/LCMRange.cpp
@@ -1,3 +1,12 @@
+/*
+ * TopCoder
+ * Single Round Match: 162
+ * Division: 2
+ * Level:1
+ * Points: 250
+ * Description: https://community.topcoder.com/stat?c=problem_statement&pm=1792
+ */
+
 #include <iostream>
 
 using namespace std;
@@ -9,21 +18,19 @@ public:
     //       you use this formula to find the least common multiple (LCM):
     //                        LCM(a,b) = a*b / GCD(a,b)
     //
+    
     int lcm(int first, int last){
         int result = first;
-        
         for (int i = first; i <= last; ++i) {
             result = (result * i)/ GCD(result,i);
         }
         return result;
     }
-    
 private:
     // Euclidean algorithm to find GCD
     int GCD(int a, int b){
         return (b == 0 ? a : GCD(b, a%b));
     }
-    
 };
 
 int main() {
