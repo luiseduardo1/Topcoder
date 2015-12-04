4147e70ca352a3292d13b3e1e0d9f6fb9457853b
Luis E. Obando <luiseduardo.obando@gmail.com>
2015-12-03 23:46:35 -0500

    Add SRM 170 DIV 2 LevelUp problem

---
+++ SRM 170/DIV 2/LevelUp.cpp
@@ -0,0 +1,35 @@
+/*
+ * TopCoder
+ * Single Round Match: 170
+ * Division: 2
+ * Level:1
+ * Points: 250
+ * Description: https://community.topcoder.com/stat?c=problem_statement&pm=1930
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class LevelUp {
+public:
+    int toNextLevel(vector<int> expNeeded, int received) {
+        int experienceNeeded = 0;
+        for(int i = 0; i < expNeeded.size() ; ++i){
+            if (expNeeded[i] > received){
+                experienceNeeded = expNeeded[i] - received;
+                break;
+            }
+        }
+        return experienceNeeded;
+    }
+};
+
+int main(){
+    LevelUp testClass;
+    vector<int> vectorTest= {150,450,900,1800};
+    int testint = 612;
+    cout << testClass.toNextLevel(vectorTest, testint)<< endl;
+
+}
